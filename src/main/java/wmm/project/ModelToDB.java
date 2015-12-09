1abf29147e5850e245f4733c64a3a297b33ae4db
wmm893110988 <wangmm_1992@163.com>
2015-12-09 14:15:13 +0800

    Update ModelToDB.java

--- src/main/java/wmm/project/ModelToDB.java
+++ src/main/java/wmm/project/ModelToDB.java
@@ -7,7 +7,7 @@ import javax.persistence.Persistence;
 /**
  * 
  *@description 将model创建到数据库
- *@author <a href="mailto:zuiwoxing@gmail.com">刘德建</a>
+ *@author <a href="wangmm_1992@163.com">阿蒙</a>
  *@date  2013-8-25 上午9:51:54
  */
 public class ModelToDB {
